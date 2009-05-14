c7acb1f72a31f7f0017b4731c04497f96b3edc3e
diegotolentino <diegotolentino@a40bdec2-fe80-11dd-9d1d-173cc1d06e3c>
2009-05-14 14:13:50 +0000

    Exemplo de cnab400

---
+++ trunk/Exemplo_1_CNAB400/index.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Arquivo que contem a classe de importação, com funções de consistencia
+ * e de leitura de arquivos com base em layout pre-definido
+ */
+require_once '../km_importador.php';
+
+/**
+ * Arquivo que contem a função para interpretação
+ * de arquivos CNAB400
+ */
+require_once 'retorno_cnab400.php';
+
+/*identifica qual onde esta o arquivo de retorno*/
+$sArquivoRetorno = './ARQ_RETORNO.TXT';
+
+/*exibe a matriz*/
+echo nl2br(print_r(Retorno_CNAB400($sArquivoRetorno), true));
+?>
